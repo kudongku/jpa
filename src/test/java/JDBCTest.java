e0015b90c189a6e421f36ce9cf490f4e60367748
kudongku <kudongku@naver.com>
2024-03-04 19:20:30 +0900

    feat: JDBC로 insert, select 실습

--- src/test/java/JDBCTest.java
+++ src/test/java/JDBCTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.sql.Connection;
@@ -25,6 +26,35 @@ public class JDBCTest {
             connection.close();
         } catch (SQLException e) {
             e.printStackTrace();
+        }
+    }
+
+    @Test
+    @DisplayName("JDBC 삽입/조회 실습")
+    void jdbcInsertSelectTest() throws SQLException {
+        // given
+        String url = "jdbc:postgresql://localhost:5432/messenger";
+        String username = "teasun";
+        String password = "pass";
+
+        // when
+        try (Connection connection = DriverManager.getConnection(url, username, password)) {
+            System.out.println("Connection created: " + connection);
+
+            String insertSql = "INSERT INTO ACCOUNT (id, username, password) VALUES (1, 'user1', 'pass1')";
+            try (PreparedStatement statement = connection.prepareStatement(insertSql)) {
+                statement.execute();
+            }
+
+            // then
+            String selectSql = "SELECT * FROM ACCOUNT";
+            try (PreparedStatement statement = connection.prepareStatement(selectSql)) {
+                var result = statement.executeQuery();
+                while (result.next()) {
+                    System.out.printf("%d, %s, %s%n", result.getInt(1), result.getString(2),
+                            result.getString(3));
+                }
+            }
         }
     }
 }
