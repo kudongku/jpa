07c1b6591453beb3e3f3e87bc15d3ddee5d6cbf5
kudongku <kudongku@naver.com>
2024-03-07 15:40:22 +0900

    feat: PageAblE 사용하기

---
+++ src/main/java/me/dongku/jpa/common/PageDto.java
@@ -0,0 +1,19 @@
+package me.dongku.jpa.common;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class PageDto {
+    private final Integer currentPage;
+    private final Integer size;
+    private String sortBy;
+    public Pageable toPageable(){
+        return PageRequest.of(currentPage-1, size, Sort.by(sortBy).descending());
+    }
+}
--- src/test/java/me/dongku/jpa/my/MyRepositoryImplTest.java
+++ src/test/java/me/dongku/jpa/my/MyRepositoryImplTest.java
@@ -1,11 +1,13 @@
 package me.dongku.jpa.my;
 
+import me.dongku.jpa.common.PageDto;
 import me.dongku.jpa.user.User;
 import me.dongku.jpa.user.UserRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
@@ -38,5 +40,24 @@ class MyRepositoryImplTest {
         var userNameList = userRepository.findByUsername("test01", Sort.by("customField"));
 
         Assertions.assertEquals(true, userNameList.containsAll(List.of(user, user2)));
+    }
+
+    @Test
+    void pageDtoTest(){
+        // given
+        User user = User.builder().username("test01").password("test01p").build();
+        userRepository.save(user);
+        User user2 = User.builder().username("test02").password("test02p").build();
+        userRepository.save(user2);
+        User user3 = User.builder().username("test03").password("test03p").build();
+        userRepository.save(user3);
+
+        PageDto pageDto = new PageDto(2, 2, "password");
+
+        // when
+        Page<User> page = userRepository.findAll(pageDto.toPageable());
+
+        // then
+        assert page.getContent().size()==2;
     }
 }
